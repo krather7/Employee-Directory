230613c5e9272e4625e1d03714250824bd3241fa
Kyle Rather <krather7@gmail.com>
2021-06-25 09:56:32 -0500

    Add files via upload

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,10 +5,6 @@ const Header = () => {
   return (
     <header>
       <h1 className="text-center">Employee Directory</h1>
-      <p className="text-center">
-        Click on column headers to filter by heading or use the search box to narrow
-        your results.
-      </p>
     </header>
   );
 };
